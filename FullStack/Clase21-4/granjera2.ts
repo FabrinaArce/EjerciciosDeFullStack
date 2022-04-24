503690f3ccaf3f14ba3d2b62e5054338f685990a
FabrinaArce <104107228+FabrinaArce@users.noreply.github.com>
2022-04-23 21:26:31 -0300

    tarea 21-04-22

---
+++ FullStack/Clase21-4/granjera2.ts
@@ -0,0 +1,11 @@
+console.log("La granjera 2:");
+console.log("avanzar");
+console.log("llenar");
+console.log("avanzar");
+console.log("avanzar");
+console.log("avanzar");
+console.log("llenar");
+console.log("avanzar");
+console.log("eliminar");
+console.log("avanzar");
+console.log("eliminar");
